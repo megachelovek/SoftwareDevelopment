a20146bce7a87513a7419d0c99819975cabe061a
Danila Malchikov <2regvk@mail.ru>
2019-12-10 22:31:17 +0400

    [*] #master security

---
+++ src/main/java/com/ssau/demo/Security/UserPrincipal.java
@@ -0,0 +1,96 @@
+package com.ssau.demo.Security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ssau.demo.Entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserPrincipal implements UserDetails {
+  private Integer id;
+
+  private String username;
+  @JsonIgnore
+  private String password;
+
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public UserPrincipal(Integer id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.authorities = authorities;
+  }
+
+  public static UserPrincipal create(UserEntity user) {
+    List<GrantedAuthority> authorities = user.getRoles().stream()
+            .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+            .collect(Collectors.toList());
+
+    return new UserPrincipal(
+            user.getId(),
+            user.getUsername(),
+            user.getPassword(),
+            authorities
+    );
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+    this.authorities = authorities;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
